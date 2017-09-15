79188ea32af569130aeca7a8a0035fde927990f7
alumno <alumno>
2017-09-14 22:13:51 -0300

    Clase 14-09 http y formValidators

--- Clases/Clase 14-09/TP_Lab4_2017/src/app/app.module.ts
+++ Clases/Clase 14-09/TP_Lab4_2017/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
 import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
 import { LoginComponent } from './componentes/login/login.component';
-import { HttpModule } from '@angular/http';
-
 //  import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 // import { AccordionModule } from 'ngx-bootstrap';
@@ -36,16 +34,13 @@ const MiRuteo = [{path: 'error' , component: ErrorComponent},
 */
 
 import { JuegoServiceService } from './servicios/juego-service.service';
-import { MiHttpService } from './servicios/mi-http.service';
 import { ListadosComponent } from './componentes/listados/listados.component';
 import { JuegosComponent } from './componentes/juegos/juegos.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { MenuCardComponent } from './componentes/menu-card/menu-card.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
-import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
-
-
+import { AnagramaComponent } from './componentes/anagrama/anagrama.component'
 
 @NgModule({
   declarations: [
@@ -72,13 +67,13 @@ import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
     BrowserModule,
     FormsModule,
     RuteandoModule,
-    ReactiveFormsModule,
-    HttpModule
+    ReactiveFormsModule
+
     // NgbModule.forRoot(MiRuteo),
     // importo el ruteo
     // RouterModule.forRoot(MiRuteo)
   ],
-  providers: [ JuegoServiceService,MiHttpService],
+  providers: [ JuegoServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
