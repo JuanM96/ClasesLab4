70bebb03833b26ceb96ce769b35801edc99f4869
alumno <alumno>
2017-11-02 20:35:16 -0300

    Smart Table

--- Clases/Clase_26-10/Resuelto/SalaDeJuegos26-10/SalaDeJuegos/src/app/componentes/jugadores-listado/jugadores-listado.component.ts
+++ Clases/Clase_26-10/Resuelto/SalaDeJuegos26-10/SalaDeJuegos/src/app/componentes/jugadores-listado/jugadores-listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JugadoresService } from '../../servicios/jugadores.service';
+import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 @Component({
   selector: 'app-jugadores-listado',
   templateUrl: './jugadores-listado.component.html',
@@ -14,10 +15,45 @@ export class JugadoresListadoComponent implements OnInit {
       this.miJugadoresServicio = serviceJugadores;
       
     }
+    settings = {
+      mode:'in-line',
+      columns: {
+        usuario: {
+          title: 'Usuario'
+        },
+        cuit: {
+          title: 'Cuit'
+        },
+        sexo: {
+          title: 'Sexo'
+        },
+        fecha: {
+          title: 'Fecha'
+        },
+        gano: {
+          title: 'Gano'
+        }
+      },
+      add:{
+        confirmCreate: true
+      },
+      edit:{
+        editConfirm: true
+      },
+      delete:{
+        deleteConfirm: true
+      },
+      actions:{
+        edit:true,
+        add:true,
+        delete:true
+      }
+    };
     
 
 
   ngOnInit() {
+    this.TraerTodos();
   }
 
 
@@ -40,5 +76,16 @@ export class JugadoresListadoComponent implements OnInit {
       //console.info("jugadores listado",(data));
       this.listado= data;
   })}
-
+  Creando(data){
+    console.info(data);
+    data.confirm.resolve();
+  }
+  Editando(data){
+    console.info(data);
+    data.confirm.resolve();
+  }
+  Borrando(data){
+    console.info(data);
+    data.confirm.resolve();
+  }
 }
